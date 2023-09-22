9441247986edbef401bbde13d7074371589184c3
Collin Dapper <collindapper@gmail.com>
2023-09-22 13:02:08 -0700

    initial commit

---
+++ app/javascript/src/home.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Layout from './layout';
+
+import './home.scss';
+
+class Home extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      userProjects: [],
+    }
+  }
+
+  render () {
+    return (
+      <Layout>
+        <div>
+          <h1>Weather App Testing</h1>
+        </div>
+      </Layout>
+    )
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <Home />,
+    document.body.appendChild(document.createElement('div')),
+  )
+})
